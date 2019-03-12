b1a721487f65455dbb7dd52c59657fb68ef38c5a
Kirill Marchuk <62mkv@mail.ru>
2019-03-12 21:04:55 +0200

    added check to avoid re-processing when files are identical

--- run-import.bat
+++ run-import.bat
@@ -1,2 +1,4 @@
+fc /b %USERPROFILE%\OneDrive\Документы\доходы_расходы.xls source-data\доходы_расходы.xls >NUL
+IF %ERRORLEVEL% EQU 0 EXIT 0
 copy %USERPROFILE%\OneDrive\Документы\доходы_расходы.xls source-data 
 python script.py %1 %2 %3
